c643c2bac31abae1e25b7776d1a4d77667d77919
technicalxakash <akashr2468@gmail.com>
2024-10-20 00:26:41 +0530

    new

---
+++ ecommerceapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from ecommerceapp import views
+
+urlpatterns = [
+ 
+ path('',views.index,name="index"),
+ path('contact',views.contact,name="contact"),
+ path('about',views.about,name="about"),
+ path('profile',views.profile,name="profile"),
+ path('checkout/', views.checkout, name="Checkout"),
+ path('handlerequest/', views.handlerequest, name="HandleRequest"),
+ path('checkout/place/',views.place,name='place'),
+ 
+
+]
